14d30b90bc0dadb75260408d464f531d206e5b2c
Mehmet CEYLAN <ceylarino@gmail.com>
2021-02-14 00:48:14 +0300

    Add: hw 5

---
+++ c7_GenericsPractice/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c7_GenericsPractice
+{
+    class MyDictionary<T, U>
+    {
+        T[] keys;
+        U[] values;
+
+        // constructor
+        public MyDictionary()
+        {
+            // Arrays are initialized empty
+            keys = new T[0];
+            values = new U[0];
+        }
+
+        public void Add(T key, U value)
+        {
+            // Keeping previous values
+            T[] tempKey = keys;
+            U[] tempValue = values;
+
+            // Recreate by increasing capacities by 1
+            keys = new T[keys.Length + 1];
+            values = new U[values.Length + 1];
+
+            // Copy previous
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                keys[i] = tempKey[i];
+                values[i] = tempValue[i];
+            }
+
+            // Add new key-value at last index
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+
+    }
+}
---
+++ c7_GenericsPractice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace c7_GenericsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+            myDictionary.Add("One", 1);
+            myDictionary.Add("Two", 2);
+            myDictionary.Add("Three", 3);
+            myDictionary.Add("Four", 4);
+            myDictionary.Add("Five", 5);
+
+        }
+    }
+}
